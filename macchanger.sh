c3ba0aec5a490da7005136dda25a92124813f6bc
danieleperera <danieleperera@gmail.com>
2018-08-13 14:03:28 +0200

    First bash script

---
+++ macchanger.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+echo Script per cambiare mac-address !
+
+ROOT_UID=0 
+# Da eseguire come root, naturalmente.
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+echo "Devi essere root per eseguire questo script."
+exit $E_NONROOT
+fi
+
+#stampa il mac address attuale
+ifconfig | grep ether
+echo 
+
+
+ifconfig wlo1 down
+macchanger -r wlo1
+ifconfig wlo1 up
+dhclient -4 wlo1
+
+echo
+#stampa mac address modificato
+ifconfig | grep ether
+echo
+
+exit
